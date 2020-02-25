f7aa065b7d17f19ab3cf5a1b424a9562b9ba82af
revathynattamai <revathy.ng@gmail.com>
2020-02-25 21:35:35 +0530

    Objects

--- main.js
+++ main.js
@@ -3,7 +3,22 @@ const todos = [
     { value: "Open", "onclick": "CreateNewDoc()" },
     { value: "Close", "onclick": "CloseDoc()" }
 ]
-
 let a = todos.filter(todo => todo.onclick === "CreateNewDoc()").map(todo => todo.value)
-
 console.log(a);
+
+function Person(a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+} 
+// ES6 does not work in prototype
+// Person.prototype.getFullName = () => { 
+//     return `${this.a} ${this.b} ${this.c}`;
+// }
+Person.prototype.getFullName = function () { 
+    return `${this.a}${this.b} ${this.c}`;
+}
+
+
+const person1 = new Person("Reva", "thy", "Karthik");
+console.log(person1.getFullName());
